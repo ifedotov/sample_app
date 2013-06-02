138bfd7592829c27a476fde058f4683a99465da4
ifedotov <ilya@wholelattelove.com>
2013-06-02 16:42:02 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -72,7 +76,7 @@ describe User do
       @user.reload.email.should == mixed_case_email.downcase
     end
   end
-  
+
 
 
   describe "when password is not present" do
@@ -112,8 +116,9 @@ describe User do
     end
   end
 
-
-
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
